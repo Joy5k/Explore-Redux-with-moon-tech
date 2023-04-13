7ab2300b46bb3e0dfefa076a7f93baa5c6108b09
Joy <mmehedihasanjoyv@gmail.com>
2023-04-14 01:16:26 +0600

    show the quantity and more

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
-import { useProducts } from "../context/ProductProvider";
-import { actionTypes } from "../state/ProductState/actionTypes";
-import { addToCart,removeFromCart } from "../features/cart/cartSlice";
+import { useLocation } from "react-router-dom";
+import { MdDeleteForever } from "react-icons/md";
 import { useDispatch } from "react-redux";
+import { addToCart, removeFromCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
-const dispatch=useDispatch()
+  const { pathname } = useLocation();
+  const dispatch = useDispatch();
   return (
-    <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key={product._id}
-    >
+    <div className='shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900'>
+      {pathname.includes("cart") && (
+        <div className='rounded-full grid place-items-center absolute top-2 right-2 bg-indigo-500 text-white h-8 w-8 font-bold '>
+          <p> {product.quantity} </p>
+        </div>
+      )}
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
       </div>
@@ -20,32 +23,44 @@ const dispatch=useDispatch()
       <div className=' flex-1'>
         <ul className='space-y-2'>
           {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+            return (
+              <li key={feature} className='text-sm '>
+                {feature}
+              </li>
+            );
           })}
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-      <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          onClick={() =>
-            dispatch(addToCart(product))
-          }
-        >
-          Add to cart
-        </button>
-        <button
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-          onClick={() =>
-            dispatch(removeFromCart(product))
-          }
-        >
-      Remove   </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
-        </button>
+        {!pathname.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+          >
+            Add to cart
+          </button>
+        )}
+
+        {!pathname.includes("cart") && (
+          <button
+        
+            title='Add to wishlist'
+            className='bg-indigo-500  py-1 px-2 rounded-full'
+          >
+            <BiListPlus className='text-white' />
+          </button>
+        )}
+        {pathname.includes("cart") && (
+          <button
+          onClick={()=>dispatch(removeFromCart(product))}
+            title='Remove'
+         
+            className='flex justify-between px-3 bg-red-500 text-white p-1 rounded-full flex-1'
+          >
+            <p>Remove</p>
+            <MdDeleteForever size='25' />
+          </button>
+        )}
       </div>
     </div>
   );
