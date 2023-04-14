43b86f34a6d6b939da5e2aa2ede5a348d9435ff0
Joy <mmehedihasanjoyv@gmail.com>
2023-04-15 02:51:35 +0600

    created the brand navbar and implement the slice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
+import { useDispatch } from "react-redux";
+import filterSlice, { toggle, toggleBrands } from "../features/filter/filterSlice";
 
 const Home = () => {
+  const dispatch=useDispatch()
   const {
     state: { products, loading, error },
   } = useProducts();
+  const activeClass = "text-white bg-indigo-500 border-white";
+  const { brands,stock } = filterSlice;
 
   let content;
 
@@ -26,11 +31,50 @@ const Home = () => {
       <ProductCard key={product._id} product={product} />
     ));
   }
-
+  if (products && (stock || brands)) {
+    content = products
+      .filter((product) => {
+        if (stock) {
+          return product.status === true;
+        }
+        return product;
+      })
+      .filter((product) => {
+        if (brands.length) {
+          return brands.includes(product.brand);
+        }
+        return product;
+      })
+      .map((product) => <ProductCard key={product.model} product={product} />);
+  }
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+    <div className='max-w-7xl gap-14 mx-auto my-10'>
+    <div className='mb-10 flex justify-end gap-5'>
+      <button
+        onClick={() => dispatch(toggle())}
+        className={`border px-3 py-2 rounded-full font-semibold ${
+          stock ? activeClass : null
+        } `}
+      >
+        In Stock
+      </button>
+      <button
+        onClick={() => dispatch(toggleBrands("amd"))}
+        className={`border px-3 py-2 rounded-full font-semibold `}
+      >
+        AMD
+      </button>
+      <button
+        onClick={() => dispatch(toggleBrands("intel"))}
+        className={`border px-3 py-2 rounded-full font-semibold `}
+      >
+        Intel
+      </button>
+    </div>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
       {content}
     </div>
+  </div>
   );
 };
 
