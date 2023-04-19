e15f889ca7b5e8e78c191134a89eb616aabca153
Joy <mmehedihasanjoyv@gmail.com>
2023-04-20 02:44:34 +0600

    created the axios functionality

--- src/features/products/productAPI.js
+++ src/features/products/productAPI.js
@@ -1,6 +1,8 @@
-export const fetchProducts =async () => {
-    const res =await fetch("http://localhost:5000/products")
-    const data = await res.json()
-    return data.data;
+import axios from "../../utils/axios.config";
+
+
+export const fetchProducts = async () => {
+    const data = await axios.get('/products')
+    return data.data.data;
    
 }
--- src/utils/axios.config.js
+++ src/utils/axios.config.js
@@ -1,5 +1,16 @@
 import axios from "axios";
 
+let URL;
+switch (process.env.local) {
+    case "DEVELOPMENT":
+        URL = "http://localhost:5000/"
+        break;
+    case "production":
+        URL = "productionserver";
+
+        default:URL = "http://localhost:5000/"
+}
+
 const instance = axios.create({
     baseURL:"http://localhost:5000/"
 })
